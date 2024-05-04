04f412926b867077dec959086b8e0e83dced0924
Romanus <38374971+Romanus101@users.noreply.github.com>
2024-05-04 19:19:27 +0300

    Add files via upload

---
+++ Randbuttons/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <random>
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    std::cout << "Program is working. Start playing";
+    char arr[12] = {
+        'Q','W','E','R','Z','X','C','F','D','V','B','N'
+    };
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1, 12);
+    for(int i = 0;i<100000;i++){
+    int n = distribution(generator);
+    Sleep(n * 1000);
+    keybd_event(arr[n], 0, 0, 0);
+    keybd_event(arr[n], 0, KEYEVENTF_KEYUP, 0);
+    }
+    std::cout << "Program is stopped.Restart if you need";
+}
