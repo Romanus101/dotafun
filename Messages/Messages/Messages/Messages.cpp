794cf9b05855c3e0376c9967d2ab0c1d53935f27
Romanus <38374971+Romanus101@users.noreply.github.com>
2024-10-27 15:19:01 +0300

    Add files via upload

---
+++ Messages/Messages/Messages/Messages.cpp
@@ -0,0 +1,62 @@
+﻿#include <windows.h>
+#include <random>
+#include <iostream>
+
+// Функция обратного вызова для сворачивания всех окон
+BOOL CALLBACK MinimizeAllWindows(HWND hwnd, LPARAM lParam) {
+    // Проверяем, что окно видимое и его можно свернуть
+    if (IsWindowVisible(hwnd) && !(GetWindowLong(hwnd, GWL_STYLE) & WS_DISABLED)) {
+        ShowWindow(hwnd, SW_MINIMIZE);  // Сворачиваем окно
+    }
+    return TRUE;  // Продолжаем перечисление окон
+}
+
+void MinimizeAllVisibleWindows() {
+    EnumWindows(MinimizeAllWindows, 0);  // Сворачиваем все окна
+}
+
+int DisplayResourceNAMessageBox() {
+    // Сворачиваем все окна перед выводом сообщения
+    MinimizeAllVisibleWindows();
+
+    // Выводим окно сообщения поверх всех окон
+    int msgboxID = MessageBox(
+        NULL,
+        (LPCWSTR)L"Attention, stretch out, do an eye workout and straighten your back",
+        (LPCWSTR)L"Important information",
+        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2 | MB_TOPMOST // Устанавливаем поверх всех окон
+    );
+
+    // Обработка ответа пользователя
+    switch (msgboxID) {
+    case IDCANCEL:
+        // TODO: добавить код, если нужно
+        break;
+    case IDTRYAGAIN:
+        DisplayResourceNAMessageBox();
+        break;
+    case IDCONTINUE:
+        DisplayResourceNAMessageBox();
+        break;
+    }
+
+    return msgboxID;
+}
+
+int main() {
+    std::cout << "Program is working. Start playing" << std::endl;
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1, 12);
+
+    // Цикл с уведомлениями
+    for (int i = 0; i < 100000; i++) {
+        int n = distribution(generator);
+        Sleep(n * 1500);  // Задержка
+
+        // Показываем окно уведомления
+        DisplayResourceNAMessageBox();
+    }
+
+    std::cout << "Program is stopped. Restart if you need." << std::endl;
+    return 0;
+}
