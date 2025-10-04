c4f94523a652f8b07eda6429c2ed2069b445b4c2
Romanus <38374971+Romanus101@users.noreply.github.com>
2025-10-04 19:16:14 +0300

    Delete cookiework.js
    No need rn for this

--- cookiework.js
+++
@@ -1,8 +0,0 @@
-//cookie('test', 'tank', {'expires':1, 'Samesite': 'Strict', 'path':'', 'domain':''});
-//cookie.set({
- // password: '23332',
-//  name: 'gfgf'
-//});
-//console.log(cookie.get('test'));
-//console.log(cookie());
-//cookie.remove('test');
